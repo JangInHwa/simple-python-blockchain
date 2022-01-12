553c25a3976507716da1d7334389e3ae9a371d2d
uglyonlytoday <superjang3357@naver.com>
2022-01-12 22:30:38 +0900

    new_transaction 구현

--- blockchain.py
+++ blockchain.py
@@ -1,4 +1,4 @@
-class Blockchain(object):
+class Blockchain:
 	def __init__(self):
 		self.chain = []
 		self.current_transactions = []
@@ -6,8 +6,13 @@ class Blockchain(object):
 	def new_block(self):
 		pass
 
-	def new_transaction(self):
-		pass
+	def new_transaction(self, sender:str, recipient:str, amount:str):
+		self.current_transactions.append({
+			'sender' : sender,
+			'recipient' : recipient,
+			'amount' : amount,
+		})
+		return self.last_block['index'] + 1
 
 	@staticmethod
 	def hash(bock):
