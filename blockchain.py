8711d486d5a7dca97e0d5084671431ff126a6541
uglyonlytoday <superjang3357@naver.com>
2022-01-15 16:37:52 +0900

    implement flask endpoint base

--- blockchain.py
+++ blockchain.py
@@ -1,6 +1,8 @@
 from time import time
 import json
 import hashlib
+from uuid import uuid4
+from flask import Flask, jsonify
 
 class Blockchain:
 	def __init__(self):
@@ -49,3 +51,27 @@ class Blockchain:
 	@property
 	def last_block(self):
 		return self.chain[-1]
+
+app = Flask(__name__)
+node_indentiifier = str(uuid4()).replace('-','')
+
+blockchain = Blockchain()
+
+@app.route('/mine', methods=['GET'])
+def mine():
+	return "We'll mine a new Block"
+
+@app.route('/transactions/new', methods=['POST'])
+def new_transaction():
+	return "We'll add a new transaction"
+
+@app.route('/chain', methods=['GET'])
+def full_chain():
+	response = {
+		'chain':blockchain.chain, 
+	'length':len(blockchain.chain),
+	}
+	return jsonify(response), 200
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', port=5050)
